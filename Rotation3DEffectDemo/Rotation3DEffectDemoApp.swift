4968cfb0f405224c29ab6b907b6050bde29e5238
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-12-05 18:59:21 -0600

    Initial Commit

---
+++ Rotation3DEffectDemo/Rotation3DEffectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rotation3DEffectDemoApp.swift
+//  Rotation3DEffectDemo
+//
+//  Created by Mark Volkmann on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Rotation3DEffectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
