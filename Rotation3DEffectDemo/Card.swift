33b34afbf8bd4e04b9c6420eec28905ad7c768a7
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-12-09 19:43:03 -0600

    progress

--- Rotation3DEffectDemo/Card.swift
+++ Rotation3DEffectDemo/Card.swift
@@ -17,8 +17,12 @@ struct Card<V1, V2, V3, V4>: View
     @State private var dragAmount = CGSize.zero
     @State private var isFlipped = false
 
+    typealias Axis = (x: CGFloat, y: CGFloat, z: CGFloat)
+
+    let axis: Axis = (x: 0, y: 1, z: 0)
     let cardHeight = 300.0
     let cardWidth = 200.0
+    let cornerRadius = 20.0
     let duration: CGFloat = 0.3
 
     var body: some View {
@@ -26,49 +30,53 @@ struct Card<V1, V2, V3, V4>: View
             ZStack {
                 frontBg
                     .frame(width: cardWidth, height: cardHeight)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
                 frontFg
             }
             .rotation3DEffect(
                 Angle(degrees: frontDegrees),
-                axis: (x: 0, y: 1, z: 0)
+                axis: axis
             )
             ZStack {
                 backBg
                     .frame(width: cardWidth, height: cardHeight)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
                 backFg
             }
             .rotation3DEffect(
                 Angle(degrees: backDegrees),
-                axis: (x: 0, y: 1, z: 0)
+                axis: axis
             )
         }
         .onTapGesture {
             flip()
         }
-        .gesture(
-            DragGesture()
-                .onChanged {
-                    let size = $0.translation // type is CGSize
-                    let percent = -size.width / 80
-                    let bounded = max(min(percent, 1), 0)
-                    isFlipped = frontDegrees <= -90
-                    let delta = percent * 90
-                    if isFlipped {
-                        frontDegrees = -90
-                        backDegrees = delta
-                    } else {
-                        backDegrees = 90
-                        frontDegrees = -delta
-                    }
-                }
-                .onEnded { _ in
-                    withAnimation(.spring()) {
-                        dragAmount = .zero
-                    }
-                }
-        )
+        /*
+         .gesture(
+             DragGesture()
+                 .onChanged {
+                     let size = $0.translation // type is CGSize
+                     // TODO: Base the percentage on distance from horizontal center of card.
+                     // TODO: Treat left and right drags differently.
+                     let percent = -size.width / 80
+                     let bounded = max(min(percent, 1), 0)
+                     isFlipped = frontDegrees <= -90
+                     let delta = percent * 90
+                     if isFlipped {
+                         frontDegrees = -90
+                         backDegrees = delta
+                     } else {
+                         backDegrees = 90
+                         frontDegrees = -delta
+                     }
+                 }
+                 .onEnded { _ in
+                     withAnimation(.spring()) {
+                         dragAmount = .zero
+                     }
+                 }
+         )
+         */
     }
 
     private func flip() {
